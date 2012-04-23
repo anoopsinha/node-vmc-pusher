850750bc99376c4ac9b392273503908889461ea2
anoopsinha <anoop.k.sinha@gmail.com>
2012-04-22 17:32:53 -0700

    first working basic service hook

---
+++ README.md
@@ -0,0 +1,70 @@
+Node VMC Pusher
+===============
+
+Introduction
+------------
+
+4/22/12
+
+This is a github service hook application that works with cloudfoundry.
+
+It can be deployed as an application on cloudfoundry.com, and then
+that application can be listed as a service hook for a repository on
+github.com.  Then upon pushing to github, the service hook will be
+called and the application automatically deployed to cloudfoundry.com.
+
+
+Instructions
+------------
+
+The basic instructions involve setting up the application on
+cloudfoundry.com, setting CF authentication variables to the
+application, then adding the servicehook to your github application.
+
+
+#### Step 1. Push the application into your cloudfoundry environment. 
+
+     vmc push node-vmc-pusher
+
+You will need to pick your own a unique name for the service hook.
+
+#### Step 2. Set the environment variables on the application so that it can login to your cloudfoundry account.  
+
+     vmc env-add node-vmc-pusher CF_TARGET=http://api.cloudfoundry.com
+     vmc env-add node-vmc-pusher CF_USER=<username>
+     vmc env-add node-vmc-pusher CF_PWD=<password>
+
+
+In a future version of cloudfoundry, this mechanism will need to be re-written to use CF's oauth functionality.
+
+#### Step 3. Set the github service hook for your repository to this url.
+
+     Repository -> Admin -> Service Hooks -> PostReceive URLs
+     http://node-vmc-pusher.cloudfoundry.com/pusher
+
+
+#### Step 4.  You are ready to go.  
+
+     git push <repository>
+     vmc apps
+
+This will take the repository name as the application name and push that to cloudfoundry.com when pushed.
+
+
+Notes
+-----
+
+As of today, this service hook requires all assets (i.e., packages) to
+be part of the checked in repository. It is possible to modify this
+servicehook to try to stage packages into the repository as well
+during the execution of the hook, but because the package managers are
+not installed on cloudfoundry.com, this will require running the
+application on a self hosted server.
+
+The fixtures needs a binary copy of git and a .fixtures/tmp/ directory
+for the application to properly function.  In particular, git = Ubunto
+10.04 LTS version compatible.  And there is no compatible system tmp
+directory in cloud foundry, so the tmp directory needs to be stored
+locally.  Do not move or rename these files.
+
+
---
+++ app.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var app = express.createServer();
+var util = require('util')
+var exec = require('child_process').exec;
+
+var assert = require('assert');
+var vmcjs = require('vmcjs');
+
+var target = (process.env.CF_TARGET);
+var user = (process.env.CF_USER);
+var pwd = (process.env.CF_PWD);
+
+var fs = require('fs');
+
+function puts(error, stdout, stderr) { util.puts(stdout) }
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+
+app.post('/pusher', function(req, res){
+    console.log('post received');
+    try {
+	p = req.body.payload;
+	obj = JSON.parse(p);
+	obj.repository.url = obj.repository.url.replace("https", "git") + ".git"
+	console.log(obj.repository.url + " " + obj.repository.name);
+
+	var cmd = "cd fixtures; ls; chmod +x git; rm -rf " + obj.repository.name + "; ./git clone " + obj.repository.url +"; ls; cd ..";
+
+	console.log(cmd);
+
+	var child = exec(cmd, function(err, stdout, stderr) {
+		//console.log(stdout);
+		//console.log(stderr);
+		if (err) throw err;
+		else {
+		    console.log('successful clone');
+		    vmcPush(obj.repository.name)
+		} 
+	    });
+
+
+    } catch (err) {
+	console.log("Error:", err);
+    }
+
+    res.send('Done with post');	
+});
+
+
+app.get('/pusher', function(req, res){
+    console.log('get received');
+    console.log(req);
+    res.send('Done with get'); 
+});
+
+app.get('/', function(req, res) {
+	res.send('Hello from Cloud Foundry');
+    });
+
+
+app.listen(process.env.VCAP_APP_PORT || 3000);
+
+
+
+
+function vmcPush(appName) {
+
+    console.log('trying the push: ' + appName);
+    var vmc = new vmcjs.VMC(target, user, pwd);
+    console.log('kicked off the login');
+
+    vmc.login(function(err) {
+       console.log('successful login');	    
+       assert.equal(err, undefined, "Unexpected err in login: " + util.inspect(err));
+       var appDir = './fixtures/' + appName;
+
+       console.log(fs.readdirSync('./fixtures'));
+       console.log(fs.readdirSync('./fixtures/' + appName));
+
+
+    // delete our test app if already exists (purposely ignore any errors)
+    vmc.deleteApp(appName, function(err, data){
+        vmc.push(appName, appDir, function(err) {
+	    console.log("done with the push");
+            assert.equal(err, undefined, "Unexpected err in push: " + util.inspect(err));
+	    vmc.start(appName, function(err, data){
+		    vmc.apps(function(err, apps) {
+			    console.dir(apps);
+			    console.log("DONE!\n\n");
+
+			    vmc.apps(function(err, apps) {
+				    console.dir(apps);
+				    console.log("DONE!\n\n");
+				});
+			});
+		});
+            });
+        });
+    });
+
+}
